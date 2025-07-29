1124d3573813cd6d4abd9abac990628efb687f34
Matt Seidholz <mseidholz@gmail.com>
2025-07-29 08:02:55 -0400

    Initial Commit

---
+++ Movie Log/Movie_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_LogApp.swift
+//  Movie Log
+//
+//  Created by matt on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Movie_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
